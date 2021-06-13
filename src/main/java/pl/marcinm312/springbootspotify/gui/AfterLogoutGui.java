922b104b36e008dfe28b0aad4197019064d40bde
Marcin Michalczyk <marcinm312@outlook.com>
2021-06-13 23:33:47 +0200

    Change of Vaadin version

--- src/main/java/pl/marcinm312/springbootspotify/gui/AfterLogoutGui.java
+++ src/main/java/pl/marcinm312/springbootspotify/gui/AfterLogoutGui.java
@@ -14,9 +14,7 @@ public class AfterLogoutGui extends VerticalLayout {
 	public AfterLogoutGui() {
 
 		h1 = new H1("You have been logged out");
-
 		anchor = new Anchor("..", "Go back to the application");
-		anchor.setTarget("_top");
 
 		add(h1, anchor);
 	}
