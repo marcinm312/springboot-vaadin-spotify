0494e694e2baf1fae8e3c53e252dbe24be604f26
marcinm312 <marcin.michalczyk.312@gmail.com>
2025-02-12 23:54:56 +0100

    First PKCE config

--- src/main/java/pl/marcinm312/springbootspotify/config/WebSecurityConfig.java
+++ src/main/java/pl/marcinm312/springbootspotify/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package pl.marcinm312.springbootspotify.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,15 +8,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+	private final ClientRegistrationRepository clientRegistrationRepository;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		DefaultOAuth2AuthorizationRequestResolver authorizationRequestResolver = new DefaultOAuth2AuthorizationRequestResolver(this.clientRegistrationRepository, "/oauth2/authorization");
+		authorizationRequestResolver.setAuthorizationRequestCustomizer(OAuth2AuthorizationRequestCustomizers.withPkce());
 
 		http.authorizeRequests()
 
@@ -23,10 +35,16 @@ public class WebSecurityConfig {
 				.antMatchers("/log-out").permitAll()
 				.antMatchers("/log-out/**").permitAll()
 
-				.and().oauth2Login().permitAll()
-				.and().logout().permitAll().logoutSuccessUrl("/log-out")
+				.and()
+				.oauth2Login(
+						oauth2 -> oauth2
+								.authorizationEndpoint(authorization -> authorization
+										.authorizationRequestResolver(authorizationRequestResolver))
+				)
+				//.logout(logout -> logout.permitAll().logoutSuccessHandler(logoutSuccessHandler()))
+				.logout().permitAll().logoutSuccessUrl("/log-out").and()
 
-				.and().csrf().disable()
+				.csrf().disable()
 				.sessionManagement().maximumSessions(10000).maxSessionsPreventsLogin(false)
 				.expiredUrl("/oauth2/authorization/spotify").sessionRegistry(sessionRegistry());
 		return http.build();
@@ -41,4 +59,11 @@ public class WebSecurityConfig {
 	public static ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
 		return new ServletListenerRegistrationBean<>(new HttpSessionEventPublisher());
 	}
+
+	/*private LogoutSuccessHandler logoutSuccessHandler() {
+		OidcClientInitiatedLogoutSuccessHandler logoutSuccessHandler = new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
+		logoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/log-out");
+
+		return logoutSuccessHandler;
+	}*/
 }
